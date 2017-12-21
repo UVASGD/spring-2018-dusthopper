486a577c59e7aa2aff10b3c758b5c6bc5aa0a966
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2017-12-22 05:18:33 +0530

    last commit before changing pathing

--- Assets/Scripts/ManualJump.cs
+++ Assets/Scripts/ManualJump.cs
@@ -14,16 +14,15 @@ public class ManualJump : MonoBehaviour {
 		if (!GameState.mapOpen) {
 			if (Input.GetMouseButton (0)) {
 				if (timeHeld >= GameState.secondsPerJump) {
-					Vector2 directionOfCursor = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-					directionOfCursor -= (Vector2)transform.position;
-					print (directionOfCursor);
-					RaycastHit2D[] thingsIHit = Physics2D.RaycastAll ((Vector2)transform.position, directionOfCursor, GameState.maxAsteroidDistance);
+					Vector2 directionOfCursor = (Vector2)(Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position);
+					int everythingExceptAsteroids = ~LayerMask.NameToLayer ("Asteroid"); //ignore raycasts with everything except asteroids
+					RaycastHit2D[] thingsIHit = Physics2D.RaycastAll ((Vector2)transform.position, directionOfCursor, GameState.maxAsteroidDistance, everythingExceptAsteroids);
 					if (thingsIHit.Length > 1) {
-						Transform otherAsteroid = thingsIHit[1].transform;
-//						print (otherAsteroid.gameObject.name);
+						Transform otherAsteroid = thingsIHit[1].transform; //thingsIHit[0] is the asteroid we're standing on so we want the next one
 						GetComponent<Movement> ().SwitchAsteroid (otherAsteroid);
 					} else {
-//						print ("didn't hit anything");
+						print ("didn't hit anything");
+						//TODO "jump" to point in space at end of raycast and die / lose a life
 					}
 					timeHeld = 0;
 				} else {
