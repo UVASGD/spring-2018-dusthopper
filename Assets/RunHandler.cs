a7eaa25ab20989449de64a6193a97cc312063e1d
Ryan Kann <rak3me@virginia.edu>
2018-02-17 21:16:04 -0500

    Minor GameState Updates

---
+++ Assets/RunHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Call this when starting a run. More or less randomizes asteroid belt
+	public void StartRun () {
+
+	}
+
+	//Call this any time a run ends, whether due to death or returning to hub
+	public void EndRun (bool successful) {
+		if (successful) {
+			GameState.SaveGame ();
+		} else {
+					
+		}
+	}
+}
---
+++ Assets/Scripts/SaveLoadTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKeyDown (KeyCode.M)) {
+			print ("Saving!");
+			GameState.SaveGame ();
+		} else if (Input.GetKeyDown (KeyCode.N)) {
+			print ("Loading!");
+			GameState.LoadGame ();
+		} else if (Input.GetKeyDown (KeyCode.R)) {
+			GameState.maxAsteroidDistance = Random.Range (20f, 30f);
+			GameState.secondsPerJump = Random.Range (1f, 5f);
+			GameState.playerSpeed = Random.Range (0.2f, 2f);
+			GameState.maxHunger = Random.Range (20f, 50f);
+		}
+	}
+}
