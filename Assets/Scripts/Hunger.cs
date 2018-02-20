30a0bd80403170b54b215121a7a4b2778e053d73
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2018-02-19 23:00:00 -0500

    no hunger loss debug button

--- Assets/Scripts/Hunger.cs
+++ Assets/Scripts/Hunger.cs
@@ -7,12 +7,14 @@ public class Hunger : MonoBehaviour {
 	private float hunger;
 	public Color hungerBarColor; //TODO: Change display to show a colored rectangle. also make red / yellow for when close to death
 	private float hungerBarWidth;
+	private bool debugDontLoseHunger;
 	private GUIStyle gstyle;
 	public float maxHunger;
 	// Use this for initialization
 	void Start () {
 		hunger = maxHunger;
 		hungerBarWidth = Screen.width * 5 / 8;
+		debugDontLoseHunger = false;
 	}
 	
 	// Update is called once per frame
@@ -21,13 +23,20 @@ public class Hunger : MonoBehaviour {
 			hunger = 0;
 			gameObject.GetComponent<Death> ().Die ();
 		} else {
-			hunger -= GameState.deltaTime;
+			if (!debugDontLoseHunger) {
+				hunger -= GameState.deltaTime;
+			}
 			//print ("hunger: " + hunger);
 		}
 	}
 
 	void OnGUI () {
 		GUI.Box (new Rect (Screen.width * 1/2 - hungerBarWidth * 0.5f * (hunger / maxHunger), Screen.height * 7/8, hungerBarWidth * (hunger / maxHunger), 50),"");
+
+		//This is just a debug button to toggle hunger loss (in case having to worry about that is annoying for testing)
+		if (GUI.Button(new Rect(10, Screen.height - 80, 120, 30), "No hunger")) {
+			debugDontLoseHunger = !debugDontLoseHunger;
+		}
 	}
 
 	public void setHunger(float newHunger){
