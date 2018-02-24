8aa9ec4cc700d7392c28083033b5df9ce9de9be8
Damon Cestaro <31603644+improvised1@users.noreply.github.com>
2018-02-24 16:06:42 -0500

    Death Changes

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Death : MonoBehaviour {
 
+    public bool runOnce = true;
     FadeController fade;
 
     private void Start() {
@@ -13,30 +14,37 @@ public class Death : MonoBehaviour {
 
     public void Die(){
 		print ("begining death sequence");
-	}
+        fade.fadeOut(1.0F);
+        StartCoroutine(reloadScene());
+    }
 
     //Damon Work
 
-    public bool runOnce = false;
-
     private void Update() {
 
-        if (runOnce == false) {
-            if (Time.fixedTime > 5) {
-                runOnce = true;
-                print("calling death sequence");
-                fade.fadeOut(1.0F);
+        if (runOnce == true) {
+            if (Time.fixedTime > 2) {
+                print("runOnce: " + runOnce);
+                runOnce = false;
+                Die();
             } else {
-                print("Time.fixedTime: " + Time.fixedTime);
+                print("Time: " + Time.fixedTime);
             }
+        
         }
 
     }
 
-    public void reloadScene() {
+    public IEnumerator reloadScene() {
+
+        print("starting to wait 1 second before reloading scene");
+        yield return new WaitForSeconds(1.0F);
 
         print("reloading scene");
-        //Scene scene = SceneManager.GetActiveScene();
-        //SceneManager.LoadScene(scene.name);
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+        print("reload scene done, fading in");
+        fade.fadeIn(1.0F);
+
     }
 }
