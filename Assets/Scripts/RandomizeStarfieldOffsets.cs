d0b2d4e9811b82ab7752ca9ffcd5362a1062c96f
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2018-03-08 16:02:21 -0500

    better starfields

---
+++ Assets/Scripts/RandomizeStarfieldOffsets.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeStarfieldOffsets : MonoBehaviour {
+	void Start () {
+		foreach (Transform child in transform) {
+			child.position = new Vector3(Random.Range (-20f, 20f),Random.Range(-20f,20f),child.position.z);
+		}
+	}
+}
