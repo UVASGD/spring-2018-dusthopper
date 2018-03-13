5be4b243fd06138c573b74fb071c643fc6874bc4
Ryan Kann <rak3me@virginia.edu>
2018-03-12 22:58:11 -0400

    Hub Parallax

---
+++ Assets/RandomVelocity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class RandomVelocity : MonoBehaviour {
+
+	[SerializeField]
+	private float maxSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = Random.insideUnitCircle * maxSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/StayInRadius.cs
+++ Assets/Scripts/StayInRadius.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 //Finished
 public class StayInRadius : MonoBehaviour {
 	//This script bounces an asteroid off the edge of the world (which is a big circle) keeping it from drifting away
-	
+
+	public float radius = 0f;
+
+	void Awake () {
+		if (radius == 0) {
+			radius = GameState.fieldRadius;
+		}
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (transform.position.sqrMagnitude > GameState.fieldRadius * GameState.fieldRadius) {
+		if (transform.position.sqrMagnitude > radius * radius) {
 //			print ("Boom boom");
 			Vector2 vel = GetComponent<Rigidbody2D> ().velocity;
 			GetComponent<Rigidbody2D>().velocity = Vector2.Reflect (vel, -transform.position.normalized);
