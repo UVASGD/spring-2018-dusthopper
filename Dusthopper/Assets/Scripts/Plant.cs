185d346e1711e09f8207860ce1bb64179753e6b4
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2018-03-15 20:39:24 -0400

    green plants spawn food

--- Dusthopper/Assets/Scripts/Plant.cs
+++ Dusthopper/Assets/Scripts/Plant.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class Plant : MonoBehaviour {
 
 	public string myPollen;
+	public GameObject food;
+	[SerializeField]
+	private float howFarAwayToSpawnFood;
 
 	public void dispenseReward() {
 		if (myPollen == "GreenPollen") {
-			Debug.Log ("Dispense a reward");
+			//Green plant's reward is just 2-3 food spawned in a circle around it
+			Debug.Log ("green plant dispensing reward");
+			int howManyFood = Random.Range (2, 4);
+			int numSpawned = 0;
+			Vector3 spawnPos = transform.position;
+			while(numSpawned < howManyFood){
+				spawnPos += (Vector3)Random.insideUnitCircle.normalized * howFarAwayToSpawnFood;
+				if ((spawnPos - GameState.asteroid.transform.position).magnitude <= GameState.asteroid.GetComponent<AsteroidInfo> ().radius) {
+					GameObject newFood = GameObject.Instantiate (food, spawnPos, Quaternion.identity, this.transform.parent) as GameObject;
+					numSpawned++;
+				} else {
+					print ("attempt unsuccessful");
+				}
+			}
 			Destroy (this.gameObject);
 		}
 	} 
