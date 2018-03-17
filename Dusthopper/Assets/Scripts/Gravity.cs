8e2834cc882c15262863047b48ca00469bde5034
wesdaug23 <wcd5f@virginia.edu>
2018-03-17 16:21:02 -0400

    gravity
    conflicts will suck

--- Dusthopper/Assets/Scripts/Gravity.cs
+++ Dusthopper/Assets/Scripts/Gravity.cs
@@ -8,23 +8,31 @@ public class Gravity : MonoBehaviour {
 	private Rigidbody2D rb;
 	Rigidbody2D otherRB;
 	public LayerMask asteroidLayer;
-	[Range(0f, 1000f)] public float multiplier = 10f;
+	[Range(0f, 1000f)] public float multiplier = 30f;
+	private int layer;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		layer = 1 << LayerMask.NameToLayer ("Asteroid");
+		asteroidLayer.value = layer;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Collider2D[] closeAsteroids = Physics2D.OverlapCircleAll (transform.position, 30f, asteroidLayer);
+		Collider2D[] closeAsteroids = Physics2D.OverlapCircleAll (transform.position, 30f, layer);
 
 		foreach (var asteroid in closeAsteroids) {
 			if (asteroid.tag != "Hub" && asteroid != GetComponent<Collider2D>()) {
 				otherRB = asteroid.GetComponent<Rigidbody2D> ();
 				if (rb && otherRB) {
-					Vector2 forceVector = transform.position - asteroid.transform.position;
-					otherRB.AddForce (multiplier * forceVector.normalized * rb.mass * otherRB.mass / forceVector.sqrMagnitude);
+					if (asteroid.gameObject.GetComponent<AsteroidInfo> ().pulledByGrav) {
+						//print ("pulling asteroids");
+					
+						//print ("still pulling");
+						Vector2 forceVector = transform.position - asteroid.transform.position;
+						otherRB.AddForce (multiplier * forceVector.normalized * rb.mass * otherRB.mass / forceVector.sqrMagnitude);
+					}
 				}
 			}
 		}
