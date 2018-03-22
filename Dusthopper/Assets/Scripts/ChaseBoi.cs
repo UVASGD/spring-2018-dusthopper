1cc6e0d908031dd37d90dd36bc3709b75d1e2188
Bailey Reed <bar5dc@virginia.edu>
2018-03-22 19:10:34 -0400

    started files last week

---
+++ Dusthopper/Assets/Scripts/ChaseBoi.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseBoi : Animal {
+
+
+	// has color
+	// has moving around behavior of animals, until player is on asteroid
+}
