8ec6751ca4c652ef5ef06d5d9f8d282da374da05
Ryan Kann <rak3me@virginia.edu>
2018-03-29 13:50:13 -0400

    Intro scene

---
+++ Dusthopper/Assets/Intro.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+
+	State myState;
+	FadeController fade;
+
+	// Use this for initialization
+	void Start () {
+		fade = GetComponent<FadeController> ();
+	}
+
+	void Update () {
+		if (Input.anyKeyDown) {
+			fade.fadeOut(0.4f);
+			Invoke ("StartGame", 3f);
+		}
+
+//		switch (myState) {
+//		case State.fadeIn:
+//			
+//			break;
+//
+//		default:
+//		case State.full:
+//			if (Input.anyKeyDown) {
+//				fade.fadeOut(1f);
+//				Invoke ("StartGame", 3f);
+//			}
+//			break;
+//
+//		case State.fadeOut:
+//
+//			break;
+//		}
+	}
+	
+	void StartGame () {
+		UnityEngine.SceneManagement.SceneManager.LoadScene (1);
+	}
+
+	void SetState () {
+
+	}
+
+	public enum State { fadeIn, full, fadeOut };
+}
