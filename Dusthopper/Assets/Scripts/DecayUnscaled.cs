23f4aeaa7a0714715f86b554daf6a05c20b99830
Damon Cestaro <31603644+improvised1@users.noreply.github.com>
2018-03-30 20:59:41 -0400

    ReDo2: Added A decaying error message text object

--- Dusthopper/Assets/Scripts/DecayUnscaled.cs
+++ Dusthopper/Assets/Scripts/DecayUnscaled.cs
@@ -8,27 +8,38 @@ public class DecayUnscaled : MonoBehaviour {
     public float GComp;
     public float BComp;
     public float decayTimer;
+    public bool decay = false;
+    
+    /*
+     * This class will slowly decay a text object that it's attached to.
+     */
 
-	// Use this for initialization
+    //record initial colors
 	void Start () {
         RComp = GetComponent<Text>().color.r;
         GComp = GetComponent<Text>().color.g;
         BComp = GetComponent<Text>().color.b;
     }
 	
-	// Update is called once per frame
+    //Will slowly decay the text object until its completely transparent (alpha = 0)
 	void OnGUI () {
 
-        float currentAlpha = GetComponent<Text>().color.a;
-        GetComponent<Text>().color = new Color(RComp, GComp, BComp, currentAlpha - decayTimer * Time.unscaledDeltaTime);
+        if (decay) {
 
+            float currentAlpha = GetComponent<Text>().color.a;
+            GetComponent<Text>().color = new Color(RComp, GComp, BComp, currentAlpha - decayTimer * Time.unscaledDeltaTime);
+
+            if (currentAlpha == 0) {
+                decay = false;
+            }
+        }
 
 	}
 
+    //This is the primary method.  Is called from outside to initially set non-transparent, then will slowly decay back to transparent
     public void setOpaque() {
-
-        print("ran setOpaque");
         GetComponent<Text>().color = new Color(RComp, GComp, BComp, 1f);
+        decay = true;
     }
 
 
