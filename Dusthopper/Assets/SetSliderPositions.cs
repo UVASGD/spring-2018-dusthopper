f0f5394db450706fde94093451fa3e8f64fc7a36
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2018-04-12 19:37:05 -0400

    sliders work checkboxes dont

---
+++ Dusthopper/Assets/SetSliderPositions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetSliderPositions : MonoBehaviour {
+
+	public Slider fXSlider;
+	public Slider musicSlider;
+	public Slider scrollSpeedSlider;
+
+	void Start(){
+		fXSlider.value = PlayerPrefs.GetFloat ("FX");
+		musicSlider.value = PlayerPrefs.GetFloat ("MUSIC");
+		scrollSpeedSlider.value = PlayerPrefs.GetFloat ("SCROLL");
+	}
+}
